77c4999f4e1696717d2e384d6b2f87207c614992
Michael Drobot <mdrobot7@iastate.edu>
2025-11-12 10:14:24 -0600

    top_level: Finish vertex shader for full triangle

--- verilog/dv/top_level/src/asm/vertex_shader.s
+++ verilog/dv/top_level/src/asm/vertex_shader.s
@@ -14,60 +14,83 @@
 ; unrolled for better performance. Bunching up memory
 ; accesses will result in better cache hit rates.
 
-; Thread ID (tid) is loaded into r0 by the core controller
-start:
-() add $r1, $g46, $tid
+; Thread ID (tid) is loaded into r0 by the core controller.
+; Calculate the memory address of our triangle.
+() muli $tid, $tid, 4
+() add $tid, $tid, $g46
 
 ; Load triangle indices
-() lw $r2, 0[$r1]
-() lw $r3, 4[$r1]
-() lw $r4, 8[$r1]
+() lw $r1, 0[$tid]
+() lw $r2, 4[$tid]
+() lw $r3, 8[$tid]
+() muli $r1, $r1, 20
+() muli $r2, $r2, 20
+() muli $r3, $r3, 20
 
 ; Load vertex 1
-() lw $r5, 0[$r2]
-() lw $r6, 4[$r2]
-() lw $r7, 8[$r2]
-() andi $r8, $r8, 0 ; Add homogeneous term
-() ori $r8, $r8, 1
-() lw $r9, 12[$r2]
-() lw $r10, 16[$r2]
+() lw $r4, 0[$r1] ; vx
+() lw $r5, 4[$r1] ; vy
+() lw $r6, 8[$r1] ; vz
+() andi $r7, $r7, 0 ; Homogeneous term
+() ori $r7, $r7, 1
+() lw $r12, 12[$r1] ; tx
+() lw $r13, 16[$r1] ; ty
 
 ; Vertex shade 1
 () dot4 $g0, $r5
-() macrd $r11
+() macrd $r8
 () dot4 $g4, $r5
-() macrd $r12
+() macrd $r9
 () dot4 $g8, $r5
-() macrd $r13
+() macrd $r10
 () dot4 $g12, $r5
-() macrd $r14
+() macrd $r11
 
-; TODO: Ship this out to the rasterizer somehow
+; Ship to rasterizer
+() out $r8
 
 ; Load vertex 2
-() lw $r5, 0[$r3]
-() lw $r6, 4[$r3]
-() lw $r7, 8[$r3]
-() andi $r8, $r8, 0 ; Add homogeneous term
-() ori $r8, $r8, 1
-() lw $r9, 12[$r3]
-() lw $r10, 16[$r3]
+() lw $r4, 0[$r2]
+() lw $r5, 4[$r2]
+() lw $r6, 8[$r2]
+() andi $r7, $r7, 0
+() ori $r7, $r7, 1
+() lw $r12, 12[$r2]
+() lw $r13, 16[$r2]
 
 ; Vertex shade 2
 () dot4 $g0, $r5
+() macrd $r8
+() dot4 $g4, $r5
+() macrd $r9
+() dot4 $g8, $r5
+() macrd $r10
+() dot4 $g12, $r5
 () macrd $r11
 
+; Ship to rasterizer
+() out $r8
+
 ; Load vertex 3
-() lw $r5, 0[$r4]
-() lw $r6, 4[$r4]
-() lw $r7, 8[$r4]
-() andi $r8, $r8, 0 ; Add homogeneous term
-() ori $r8, $r8, 1
-() lw $r9, 12[$r4]
-() lw $r10, 16[$r4]
+() lw $r4, 0[$r3]
+() lw $r5, 4[$r3]
+() lw $r6, 8[$r3]
+() andi $r7, $r7, 0
+() ori $r7, $r7, 1
+() lw $r12, 12[$r3]
+() lw $r13, 16[$r3]
 
 ; Vertex shade 3
 () dot4 $g0, $r5
+() macrd $r8
+() dot4 $g4, $r5
+() macrd $r9
+() dot4 $g8, $r5
+() macrd $r10
+() dot4 $g12, $r5
 () macrd $r11
 
-j start
+; Ship to rasterizer
+() out $r8
+
+halt

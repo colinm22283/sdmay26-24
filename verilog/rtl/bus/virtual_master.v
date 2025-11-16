8864f5893f0e92670e9a52a5c2856626cc58d465
Colin McGann <colinm22@iastate.edu>
2025-11-16 13:01:17 -0600

    Yep

--- verilog/rtl/bus/virtual_master.v
+++ verilog/rtl/bus/virtual_master.v
@@ -32,6 +32,28 @@ module virtual_master_m(
     end
     endtask
 
+    task WRITE_WORD;
+        input [`BUS_ADDR_PORT] addr;
+        input [31:0] data;
+    begin
+        wait(!clk_i);
+
+        mport_o[`BUS_MO_ADDR] = addr;
+        mport_o[`BUS_MO_DATA] = data;
+        mport_o[`BUS_MO_SIZE] = `BUS_SIZE_WORD;
+        mport_o[`BUS_MO_RW]   = `BUS_WRITE;
+        mport_o[`BUS_MO_REQ]  = 1;
+
+        wait(mport_i[`BUS_MI_ACK]);
+        wait(!mport_i[`BUS_MI_ACK]);
+
+        mport_o[`BUS_MO_REQ]  = 0;
+
+        wait(!clk_i);
+        wait(clk_i);
+    end
+    endtask
+
     task WRITE_STREAM;
         input [`BUS_ADDR_PORT] addr;
         input [`BUS_ADDR_PORT] size;

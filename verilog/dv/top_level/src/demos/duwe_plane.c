b675d1d387f5d39aeb6371b91dde61b85deeca68
Colin McGann <colinm22@iastate.edu>
2026-04-02 22:36:35 -0500

    Implemented done signal on rasterizer

--- verilog/dv/top_level/src/demos/duwe_plane.c
+++ verilog/dv/top_level/src/demos/duwe_plane.c
@@ -8,7 +8,7 @@
 #define INDEX_BUFFER_PK_ADDR  (QSPI0_MEM_ADDR_DEFAULT + 0x80000)
 
 #define NUM_VERTICES (6)
-#define NUM_INDICES  (12)
+#define NUM_INDICES  (6)
 static const uint32_t vertices[NUM_VERTICES] = {}; // TODO
 static const uint32_t indices[NUM_INDICES]   = {}; // TODO
 
